2fb855aef249349c0159e402cdc5bc8bdc508a98
xTeel-Code <119667246+xTeel-Code@users.noreply.github.com>
2026-06-15 23:14:54 +0200

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use crossterm::terminal::enable_raw_mode;
+use crossterm::terminal::disable_raw_mode;
+use std::io::{self,Read};
+fn main() {
+    enable_raw_mode().unwrap();
+    for b in io::stdin().bytes(){
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control(){
+            println!("Binary: {0:08b}, ASCII: {0:#03} \r", b)
+        }
+        else{
+            println!("Binary: {0:08b}, ASCII: {0:#03}, Character: {1:#?} \r", b ,c)
+        };
+        
+        if c == 'q'{
+            disable_raw_mode().unwrap();
+            break;
+        }
+    }
+}
