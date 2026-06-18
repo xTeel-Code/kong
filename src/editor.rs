721f7b826314984ca44c54537c1818326f69e363
xTeel-Code <119667246+xTeel-Code@users.noreply.github.com>
2026-06-18 20:40:11 +0200

    added "§" to draw at startup organised code

--- src/editor.rs
+++ src/editor.rs
@@ -1,28 +1,63 @@
-use crossterm::event::{Event::Key, KeyCode::Char, read};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-pub struct Editor {}
+use crossterm::event::{Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
+
+mod terminal;
+use terminal::Terminal;
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
-    pub fn default() -> Self {
-        Editor {}
+    pub const fn default() -> Self {
+        Editor { should_quit: false }
     }
-    pub fn run(&self) {
-        print!("\x1b[2J");
-        enable_raw_mode().unwrap();
+    pub fn run(&mut self) {
+        Terminal::initialize().unwrap();
+        let result = self.repl();
+        Terminal::terminate().unwrap();
+        result.unwrap();
+    }
+    fn draw_rows() -> Result<(), std::io::Error> {
+        let term_height = Terminal::size()?.1;
+        for current_row in 0..term_height {
+            print!("§");
+            if current_row < term_height {
+                print!("\r\n")
+            }
+        }
+        Ok(())
+    }
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{:?} \r", event);
-                    if let Char(c) = event.code
-                        && c == 'q'
-                    {
-                        break;
-                    }
+            self.refresh_screen()?;
+            if self.should_quit {
+                break;
+            }
+            let event = read()?;
+            self.evaluate_event(&event);
+        }
+        Ok(())
+    }
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+            code, modifiers, ..
+        }) = event
+        {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
                 }
-                Err(err) => println!("Error: {}", err),
                 _ => (),
             }
         }
-        disable_raw_mode().unwrap();
+    }
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        if self.should_quit {
+            Terminal::clear_screen()?;
+            print!("Goodbye.\r\n");
+        } else {
+            Self::draw_rows()?;
+            Terminal::move_cursor_to(0, 0)?;
+        }
+        Ok(())
     }
 }
